a14d684ee32e472fa514952d9f87f4b7c917e0e6
Rick Kerkhof <rick.2889@gmail.com>
2018-11-06 23:48:28 +0100

    Add PredefinedStringParameterTest

---
+++ tests/PredefinedStringParameterTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright 2018 The WildPHP Team
+ *
+ * You should have received a copy of the MIT license with the project.
+ * See the LICENSE file for more information.
+ */
+
+use PHPUnit\Framework\TestCase;
+use WildPHP\Commands\Parameters\PredefinedStringParameter;
+
+class PredefinedStringParameterTest extends TestCase
+{
+    public function testPredefinedString()
+    {
+        $predefinedStringParameter = new PredefinedStringParameter('test');
+
+        $this->assertTrue($predefinedStringParameter->validate('test'));
+        $this->assertFalse($predefinedStringParameter->validate('Test'));
+        $this->assertFalse($predefinedStringParameter->validate('ing'));
+    }
+}
